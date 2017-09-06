4eb0a473c5419ff007e74975086c0f046d3febb5
Gregorio Galante <gregogalante@gmail.com>
2017-09-06 12:36:42 +0200

    update

--- lib/templates/evnt_authentication/test/commands/authentication_commands/signup_command_test.rb
+++ lib/templates/evnt_authentication/test/commands/authentication_commands/signup_command_test.rb
@@ -179,7 +179,7 @@ module AuthenticationCommands
     ############################################################################
 
     def command_with_custom(key = nil, value = nil)
-      Authentication::SignupCommand.new(
+      AuthenticationCommands::SignupCommand.new(
         name: key == :name ? value : Faker::Name.first_name,
         surname: key == :surname ? value : Faker::Name.last_name,
         email: key == :email ? value : Faker::Internet.email,
@@ -189,7 +189,7 @@ module AuthenticationCommands
     end
 
     def command_with_password(password)
-      Authentication::SignupCommand.new(
+      AuthenticationCommands::SignupCommand.new(
         name: Faker::Name.first_name, surname: Faker::Name.last_name,
         email: Faker::Internet.email, password: password,
         password_confirmation: password
