409888417f6c2f9a85292be5591fc0ba9df24da8
Gregorio Galante <gregogalante@gmail.com>
2017-10-11 16:40:09 +0200

    fix sidekiq configuration files on sidekiq template

--- lib/templates/sidekiq/config/initializer/sidekiq.rb
+++ lib/templates/sidekiq/config/initializer/sidekiq.rb
@@ -4,15 +4,16 @@
 Sidekiq.configure_server do |config|
   # redis settings
   config.redis = { url: 'redis://localhost:6379/0' }
-  # sidekiq cron settings
-  schedule_file = 'config/sidekiq_schedule.yml'
-  if File.exist?(schedule_file)
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-  end
 end
 
 # Sidekiq client configuration.
 Sidekiq.configure_client do |config|
   # redis settings
   config.redis = { url: 'redis://localhost:6379/0' }
+end
+
+# Sidekiq cron configuration.
+schedule_file = 'config/sidekiq_schedule.yml'
+if File.exist?(schedule_file)
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
