fd54198e4da09f361fc87d8454d2c93f1af93c28
Gregorio Galante <gregogalante@gmail.com>
2018-04-14 13:56:39 +0200

    Updated generator.

--- README.md
+++ README.md
@@ -61,3 +61,4 @@ This command should:
 - Create a package.json inside the root of the engine.
 - Create a webpack.config.js inside the root of the engine.
 - Create a task to copy the engine assets on vendor/modules directory of the main application.
+- Update the dummy application to permits the development of the engine with webpacker.
--- lib/generators/dakarai/engine_webpacker_generator.rb
+++ lib/generators/dakarai/engine_webpacker_generator.rb
@@ -9,8 +9,28 @@ module Dakarai
 
     source_root File.expand_path('../../../../templates', __FILE__)
 
+    argument :engine, type: :string, optional: false
+
     def create_engine_webpacker
-      # TODO
+      @engine_class = engine.camelize
+      @engine_name = engine.underscore
+
+      template 'engine_webpacker/package.json', 'package.json'
+      template 'engine_webpacker/webpack.config.js', 'webpack.config.js'
+      template(
+        'engine_webpacker/lib/tasks/assets_management_tasks.rake',
+        'lib/tasks/assets_management_tasks.rake'
+      )
+      template(
+        'engine_webpacker/test/dummy/package.json',
+        'test/dummy/package.json'
+      )
+
+      copy_file 'engine_webpacker/.babelrc', '.babelrc'
+      copy_file(
+        'engine_webpacker/app/javascript/engine/index.js',
+        "app/javascript/#{@engine_name}/index.js"
+      )
     end
 
   end
