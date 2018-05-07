ecbe5144d919083622811f5fdeab92bcdfd8fec3
Gregorio Galante <gregogalante@gmail.com>
2018-05-07 10:40:08 +0200

    Initialize configuration for engine.

---
+++ lib/generators/dakarai/engine_configuration_generator.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails/generators/base'
+
+module Dakarai
+
+  # EngineConfigurationGenerator.
+  class EngineConfigurationGenerator < Rails::Generators::Base
+
+  end
+
+end
