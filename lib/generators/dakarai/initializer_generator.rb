a7016b99051676ee23f3623a930b6568591eb65c
Gregorio Galante <me@gregoriogalante.com>
2019-09-15 19:46:01 +0200

    Update generale

--- lib/generators/dakarai/initializer_generator.rb
+++ lib/generators/dakarai/initializer_generator.rb
@@ -8,7 +8,7 @@ module Dakarai
   # Copy the base files structure to the Rails project.
   class InitializerGenerator < Rails::Generators::Base
 
-    source_root File.expand_path('../../../../templates', __dir__)
+    source_root File.expand_path('../../../../templates', __FILE__)
 
     def create_initializer
       directory './initializer', './'
