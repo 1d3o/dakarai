b28aa1ce04069da0660b005047a60fb923c22fab
Gregorio Galante <me@gregoriogalante.com>
2019-11-05 08:40:31 +0100

    Update generale

---
+++ templates/initializer/test/test_helper.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# Usage:
+# Require the module inside your test: require 'test_helper'
+
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
+module ActiveSupport
+
+  # TestCase.
+  class TestCase
+
+    protected
+
+    # This function should be used to send a request on an API.
+    # It returns the response body of the request.
+    def send_simple_request(method, request, headers: {}, log: false)
+      return if !method || !request
+
+      if log
+        time = Benchmark.ms { send(method, request, headers: headers) }
+        puts "> #{self.class.name}.#{method_name} --> #{time.round(2)} ms"
+      else
+        send(method, request, headers: headers)
+      end
+
+      ::JSON.parse(response.body, symbolize_names: true)
+    end
+
+  end
+
+end
